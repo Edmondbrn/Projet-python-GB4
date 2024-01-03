235f5a0da0a0412e7a4c386897ce558798d5f1c0
Edmondbrn <edmond.berne6@gmail.com>
2024-01-03 20:25:16 +0100

    chemin linux

---
+++ Chemin_fichier_sh_Linux_MacOS.py
@@ -0,0 +1,4 @@
+import os
+os.chdir(os.path.join(os.path.dirname(__file__)))
+print(os.getcwd())
+input("Récupérez ce chemin pour exécuter le fichier .sh comme dans le README.")
--- Executable.py
+++
@@ -1,5 +0,0 @@
-import os
-os.chdir(os.path.join(os.path.dirname(__file__), "Script"))
-print(os.getcwd())
-
-from Script.application import *
